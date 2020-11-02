028800e5b7ba29e0430d15bee4baa0ae590f5809
Till Mahlburg <mahlburg@posteo.de>
2020-11-02 18:03:48 +0100

    add pydoc style documentation

--- tfm/bookmarks.py
+++ tfm/bookmarks.py
@@ -1,9 +1,22 @@
 # This Python file uses the following encoding: utf-8
 import os
 
+from typing import List, Dict
+
 
 class bookmarks:
+    """
+    Handles the bookmarks as file and as list of dicts.
+    """
+
     def __init__(self, path_to_bookmark_file: str):
+        """
+        Reads bookmarks from file to internal list of dicts or creates the
+        file if it doesn't exist yet.
+        :param path_to_bookmark_file: Path to the file, in which the bookmarks
+                                      are saved
+        :type path_to_bookmark_file: str
+        """
         self.list = []
         if os.path.exists(path_to_bookmark_file):
             self.list = self.get_bookmarks_from_file(path_to_bookmark_file)
@@ -11,7 +24,15 @@ class bookmarks:
             open(path_to_bookmark_file, 'a').close()
         self.path_to_bookmark_file = path_to_bookmark_file
 
-    def get_bookmarks_from_file(self, path):
+    def get_bookmarks_from_file(self, path: str) -> List[Dict[str, str]]:
+        """
+        Gets bookmarks from a file.
+        :param path: Path to the file, in which the bookmarks are saved.
+        :type path: str
+        :return: List of dicts containing name and path of the bookmarks.
+                 The dicts follow this structure: {'name': '', 'path': ''}
+        :rtype: List[Dict[str, str]]
+        """
         list = []
         with open(path) as bookmarks:
             for bookmark in bookmarks:
@@ -20,6 +41,17 @@ class bookmarks:
         return list
 
     def add_bookmark(self, name: str, path: str):
+        """
+        Adds a new bookmark. Does nothing, if another bookmark with the same
+        name already exists or the | character is in the chosen name. The
+        bookmark will be saved to the bookmark file that was chosen on
+        instantiation of this class.
+        :param name: The name of the new bookmark. This will be shown in the
+                     UI.
+        :type name: str
+        :param path: Path which the bookmark should lead to.
+        :type path: str
+        """
         # TODO: error handling
         if not self.bookmark_exists(name) and '|' not in name:
             self.list.append({'name': name, 'path': path})
@@ -27,6 +59,12 @@ class bookmarks:
                 bookmarks.write(name + '|' + path + '\n')
 
     def remove_bookmark(self, name: str):
+        """
+        Removes a bookmark from the internal bookmark list. It also rewrites
+        the updated list to the bookmark file chosen on class instatiation.
+        :param name: The name of the bookmark that should be deleted.
+        :type name: str
+        """
         new_list = []
         for bookmark in self.list:
             if bookmark['name'] != name:
@@ -40,16 +78,38 @@ class bookmarks:
                                 + '\n')
 
     def get_path_from_name(self, name: str):
+        """
+        Returns the path that is registered with the given bookmark name. If
+        the name doesn't exist, it returns False.
+        :param name: The of the bookmark to which the path is requested.
+        :type name: str
+        :return: The path registered with the given bookmark name, or False,
+                 if the name doesn't exist
+        :rtype: str | bool
+        """
         for bookmark in self.list:
             if name == bookmark['name']:
                 return bookmark['path']
         return False
 
-    def bookmark_exists(self, name: str):
+    def bookmark_exists(self, name: str) -> bool:
+        """
+        Returns True if the bookmark with the given name exists and False, if
+        it doesn't.
+        :param name: Name of the bookmark to check for.
+        :type name: str
+        :return: True if bookmark exists, False if it doesn't.
+        :rtype: bool
+        """
         for bookmark in self.list:
             if name == bookmark['name']:
                 return True
         return False
 
-    def get_all(self):
+    def get_all(self) -> List[Dict[str, str]]:
+        """
+        :return: List of dicts of all bookmarks. The dicts follows the format
+                 {'name': '', 'path': ''}
+        :rtype: List[Dict[str, str]]
+        """
         return self.list
--- tfm/stack.py
+++ tfm/stack.py
@@ -3,24 +3,55 @@ from collections import deque
 
 
 class stack:
+    """
+    Provides a classic stack using a deque() data structure.
+    """
     def __init__(self):
+        """
+        Initializes the stack.
+        """
         self.stack = deque()
 
-    def empty(self):
+    def empty(self) -> bool:
+        """
+        Returns True if stack is emtpy and False if it isn't.
+        :return: Emptyness of the stack.
+        :rtype: bool
+        """
         return len(self.stack) == 0
 
-    def size(self):
+    def size(self) -> int:
+        """
+        Returns the size of the stack.
+        :return: Number of elements in the stack.
+        :rtype: int
+        """
         return len(self.stack)
 
     def top(self):
+        """
+        Returns the top element of the stack.
+        :return: Top element of the stack.
+        """
         return self.stack[-1]
 
     def push(self, stack_element):
+        """
+        Adds an element to the top of the stack.
+        :param stack_element: The element which will be added to the stack.
+        """
         self.stack.append(stack_element)
 
     def pop(self):
+        """
+        Removes and returns the top element of the stack.
+        :return: Top element of the stack.
+        """
         return self.stack.pop()
 
     def drop(self):
+        """
+        Resets the entire stack by removing all of its elements.
+        """
         while (self.size() != 0):
             self.stack.pop()
