acdc9c76de20328b73581e5893b6fd5ed5ccd3e8
Till Mahlburg <mahlburg@posteo.de>
2021-05-25 01:11:46 +0200

    update to 0.0.3dev

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='tfm',
-    version='0.0.2dev',
-    packages=['tfm',],
+    version='0.0.3dev',
+    packages=['tfm', ],
     license='GPLv3',
     author='Till Mahlburg',
-    #author_email="author@example.com",
-    description='A simple and Qt file manager for *NIX',
+    # author_email="author@example.com",
+    description='A simple and Qt file manager for Linux',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/tmahlburg/tfm',
@@ -18,7 +18,8 @@ setuptools.setup(
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: X11 Applications :: Qt',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3'
+        + '(GPLv3)',
         'Natural Language :: English',
         'Operating System :: POSIX',
         'Intended Audience :: End Users/Desktop',
