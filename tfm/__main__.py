db3ce0668b883d7e760d942915b452f81fa8c336
Till Mahlburg <mahlburg@posteo.de>
2021-05-27 00:23:55 +0200

    restructure project to work as module

--- tfm/__main__.py
+++ tfm/__main__.py
@@ -4,12 +4,12 @@ import sys
 
 from PySide2.QtWidgets import QApplication
 
-from tfm.tfm import tfm
+from tfm import tfm
 
 if __name__ == "__main__":
     app = QApplication()
     # TODO: create clean CLI
     # TODO: document CLI
-    window = tfm(sys.argv)
+    window = tfm.tfm(sys.argv)
     window.show()
     sys.exit(app.exec_())
